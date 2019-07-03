fdbc42363a5916048b13be38c7d737fa0d44e960
Attila L. Egyedi <egyedia@stanford.edu>
2019-07-03 13:42:07 -0700

    Do not optimize dist

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://open.metadatacenter.orgx/',
+  cedarUrl: 'https://cedar.metadatacenter.orgx/',
+  terminologyUrl: 'https://terminology.metadatacenter.orgx/',
+  cedarFormUrl: 'https://component.metadatacenter.orgx/',
+  defaultLanguage: 'en',
+  fallbackLanguage: 'en'
+};
