c8a0fca7ddc867542eec0374ee60618948caf533
Attila L. Egyedi <attila.egyedi@gmail.com>
2020-10-12 13:09:43 -0700

    Fixed test imports, removed no longer used test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,41 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {SnotifyModule, SnotifyService, ToastDefaults} from 'ng-snotify';
+import {SpinnerComponent} from './modules/shared/components/spinner/spinner.component';
+import {NavbarComponent} from './modules/shared/components/navbar/navbar.component';
+import {FooterComponent} from './modules/shared/components/footer/footer.component';
+import {MaterialModule} from './modules/material-module';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {HttpLoaderFactory} from './app.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        SnotifyModule,
+        MaterialModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        SpinnerComponent,
+        NavbarComponent,
+        FooterComponent
       ],
+      providers: [
+        SnotifyService,
+        {provide: 'SnotifyToastConfig', useValue: ToastDefaults},
+      ]
     }).compileComponents();
   }));
 
@@ -20,16 +45,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cedar-openview'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('cedar-openview');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cedar-openview!');
-  });
 });
