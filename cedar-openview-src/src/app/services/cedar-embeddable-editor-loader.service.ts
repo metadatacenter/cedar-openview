a807638b2a63bfcaa8d8ea3c885dd3ef84e0c99d
Mete Ugur Akdogan <muakdogan@gmail.com>
2026-07-23 07:21:45 -0700

    chore: upgrade cedar-embeddable-editor to v1.5.1 and load via angular.json scripts

--- cedar-openview-src/src/app/services/cedar-embeddable-editor-loader.service.ts
+++ cedar-openview-src/src/app/services/cedar-embeddable-editor-loader.service.ts
@@ -1,24 +1,8 @@
-import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class CedarEmbeddableEditorLoaderService {
-  private loaded?: Promise<void>;
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-
   load(): Promise<void> {
-    if (!isPlatformBrowser(this.platformId)) return Promise.resolve();
-    if (!this.loaded) {
-      this.loaded = new Promise<void>((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = '/assets/third-party-components/cedar-embeddable-editor/v1.5.0/cedar-embeddable-editor.js';
-        script.defer = true;
-        script.onload = () => resolve();
-        script.onerror = () => reject(new Error('Failed to load cedar-embeddable-editor.js'));
-        document.head.appendChild(script);
-      });
-    }
-    return this.loaded;
+    return Promise.resolve();
   }
 }
